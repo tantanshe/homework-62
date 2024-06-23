abb7692a51b2b5c8b09e6014fcae4cec1dc4d92b
Tatiana Shishenina <tatyana020711@gmail.com>
2024-06-23 18:40:58 +0600

    Did basic layout for each page and added routes logic in App.tsx

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,19 @@
+import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Gallery from './Gallery';
 
-const App = () => {
-
+const App: React.FC = () => {
   return (
-    <>
-      App
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+      </Routes>
+    </div>
   );
 };
 
---
+++ src/containers/Gallery.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Gallery: React.FC = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Gallery</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin, ipsum at mollis tincidunt, leo
+        massa blandit augue, blandit rutrum urna erat eget lorem. Vestibulum ullamcorper hendrerit facilisis. Praesent
+        dignissim justo venenatis lorem lacinia, sed dignissim elit tempor. Donec facilisis ultrices libero, sit amet
+        aliquam enim posuere a. Maecenas vitae purus tempus, tempor magna sed, rhoncus leo. Suspendisse at sapien
+        sagittis elit pharetra dictum. Vivamus pulvinar sem id nunc consectetur, quis rutrum urna tincidunt. Quisque
+        lacinia risus eu ex euismod, semper commodo enim fermentum. Quisque quam enim, bibendum vel congue et,
+        scelerisque non mauris. Aenean vel est ac mauris posuere consequat tempus non lectus.
+      </p>
+    </div>
+  );
+};
+
+export default Gallery;
